3187c08940cdf058aae763c2832d074b23c2e8d7
Vadim <hellovados@gmail.com>
2022-02-09 23:21:03 +0300

    Updates

---
+++ static/js/11.fd7303be.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpemilus=this.webpackJsonpemilus||[]).push([[11],{483:function(e,s,n){"use strict";n.r(s);var t=n(1);n(0);s.default=function(){return Object(t.jsx)("div",{children:"Home component works!"})}}}]);
+//# sourceMappingURL=11.fd7303be.chunk.js.map
